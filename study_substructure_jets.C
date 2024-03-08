92f90d4a39080968e5aeb78c8e7bd760771a9cfd
Miguel Ángel García Ruíz <miguel.angel.garcia.ruiz@cern.ch>
2024-03-08 18:09:02 +0100

    Open, access and close the input files for the boosted and resolved root files

--- study_substructure_jets.C
+++ study_substructure_jets.C
@@ -5,19 +5,26 @@ void study_substructure_jets(){
   // Path of the merged samples
   
   TString path="/eos/user/g/garciarm/HHbbtautau-output-easyjet-framework/mc20_13TeV_502985_vbf_hadhad_l1cvv1p5cv1_PHYS_merged_";
-  TString name_config="boosted";
-  path+=name_config;
-  path+="_config.root";
+  TString path_boosted_sample=path+"boosted_config.root";
+  TString path_resolved_sample=path+"resolved_config.root";
 
-  TFile* inFile = TFile::Open(path);
-
-  // Access the input tree
-  TTree *inTree = (TTree*) inFile->Get("AnalysisMiniTree");
-
-  print_list_of_branches(inTree);
-
-  // Find all the branches with the words tau and jets
-  find_branches_names(inTree, name_config, "tau");
-  find_branches_names(inTree, name_config, "jet");
+  // Open the input files and access to the trees
+  TFile* boosted_inFile = TFile::Open(path_boosted_sample);
+  TTree* boosted_inTree = (TTree*) boosted_inFile->Get("AnalysisMiniTree");
   
+  TFile* resolved_inFile = TFile::Open(path_resolved_sample);
+  TTree* resolved_inTree = (TTree*) resolved_inFile->Get("AnalysisMiniTree");
+
+  // Print the 
+  print_list_of_branches(boosted_inTree);
+  print_list_of_branches(resolved_inTree);
+  
+  // Find all the branches with the words tau and jets for the boosted config
+  find_branches_names(boosted_inTree, "boosted", "tau");
+  find_branches_names(boosted_inTree, "boosted", "jet");
+
+
+  // Close the files
+  boosted_inFile->Close();
+  resolved_inFile->Close(); 
 }
