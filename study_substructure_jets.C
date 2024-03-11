568d269589e6eabd4d00b5aadbfe2d41e3de60c9
Miguel Ángel García Ruíz <miguel.angel.garcia.ruiz@cern.ch>
2024-03-11 16:37:52 +0100

    The plot and set branch address were added for the mass of the two tau graphs

--- study_substructure_jets.C
+++ study_substructure_jets.C
@@ -9,22 +9,61 @@ void study_substructure_jets(){
   TString path_resolved_sample=path+"resolved_config.root";
 
   // Open the input files and access to the trees
+
   TFile* boosted_inFile = TFile::Open(path_boosted_sample);
   TTree* boosted_inTree = (TTree*) boosted_inFile->Get("AnalysisMiniTree");
   
   TFile* resolved_inFile = TFile::Open(path_resolved_sample);
   TTree* resolved_inTree = (TTree*) resolved_inFile->Get("AnalysisMiniTree");
 
-  // Print the 
-  print_list_of_branches(boosted_inTree);
-  print_list_of_branches(resolved_inTree);
+  // Print the names of the branches in the given tree
+  
+  // print_list_of_branches(boosted_inTree);
+  // print_list_of_branches(resolved_inTree);
   
   // Find all the branches with the words tau and jets for the boosted config
-  find_branches_names(boosted_inTree, "boosted", "tau");
-  find_branches_names(boosted_inTree, "boosted", "jet");
 
+  find_branches_names(boosted_inTree, "boosted", "bbtt_H");
+  find_branches_names(resolved_inTree, "resolved", "bbtt_H");
+  
+  set_branch_address(boosted_inTree, resolved_inTree);
+  /*
+  boosted_inTree->SetBranchAddress("bbtt_H_vis_tautau_pt_NOSYS", &boosted_bbtt_H_vis_tautau_pt_NOSYS);
+  boosted_inTree->SetBranchAddress("bbtt_H_vis_tautau_m", &boosted_bbtt_H_vis_tautau_m);
+  boosted_inTree->SetBranchAddress("bbtt_H_bb_pt_NOSYS", &boosted_bbtt_H_bb_pt_NOSYS);
+  boosted_inTree->SetBranchAddress("bbtt_H_bb_m", &boosted_bbtt_H_bb_m);
+ 
+  resolved_inTree->SetBranchAddress("bbtt_H_vis_tautau_pt_NOSYS", &resolved_bbtt_H_vis_tautau_pt_NOSYS);
+  resolved_inTree->SetBranchAddress("bbtt_H_vis_tautau_m", &resolved_bbtt_H_vis_tautau_m);
+  resolved_inTree->SetBranchAddress("bbtt_H_bb_pt_NOSYS", &resolved_bbtt_H_bb_pt_NOSYS);
+  resolved_inTree->SetBranchAddress("bbtt_H_bb_m", &resolved_bbtt_H_bb_m);
+ 
 
+  boosted_inTree->SetBranchAddress("bbtt_H_vis_tautau_pt_NOSYS", &boosted_bbtt_H_vis_tautau_pt_NOSYS, &b_boosted_bbtt_H_vis_tautau_pt_NOSYS);
+  boosted_inTree->SetBranchAddress("bbtt_H_vis_tautau_m", &boosted_bbtt_H_vis_tautau_m, &b_boosted_bbtt_H_vis_tautau_m);
+  boosted_inTree->SetBranchAddress("bbtt_H_bb_pt_NOSYS", &boosted_bbtt_H_bb_pt_NOSYS, &b_boosted_bbtt_H_bb_pt_NOSYS);
+  boosted_inTree->SetBranchAddress("bbtt_H_bb_m", &boosted_bbtt_H_bb_m, &b_boosted_bbtt_H_bb_m);
+
+  
+  resolved_inTree->SetBranchAddress("bbtt_H_vis_tautau_pt_NOSYS", &resolved_bbtt_H_vis_tautau_pt_NOSYS, &b_resolved_bbtt_H_vis_tautau_pt_NOSYS) \
+    ;
+  resolved_inTree->SetBranchAddress("bbtt_H_vis_tautau_m", &resolved_bbtt_H_vis_tautau_m, &b_resolved_bbtt_H_vis_tautau_m);
+  resolved_inTree->SetBranchAddress("bbtt_H_bb_pt_NOSYS", &resolved_bbtt_H_bb_pt_NOSYS, &b_resolved_bbtt_H_bb_pt_NOSYS);
+  resolved_inTree->SetBranchAddress("bbtt_H_bb_m", &resolved_bbtt_H_bb_m, &b_resolved_bbtt_H_bb_m);
+  */
+  
+  Int_t entries = boosted_inTree->GetEntries();
+  int nbytes=0;
+
+  for(int ii = 0; ii < 50; ii++){
+    nbytes = boosted_inTree->GetEntry(ii);
+    cout << boosted_bbtt_H_vis_tautau_m << "\t" << resolved_bbtt_H_vis_tautau_m << endl; 
+  }
+  
+  plot_distributions(boosted_inTree, resolved_inTree);
+  
   // Close the files
+  
   boosted_inFile->Close();
   resolved_inFile->Close(); 
 }
--- study_substructure_jets.h
+++ study_substructure_jets.h
@@ -1,17 +1,121 @@
-// Declaration of some functions
+// *************************************
+// Declaration of leaf variables
+// *************************************
 
+// Declaration of leaf types for boosted tree
+Float_t boosted_bbtt_H_vis_tautau_pt_NOSYS;
+Float_t boosted_bbtt_H_vis_tautau_m;
+Float_t boosted_bbtt_H_bb_pt_NOSYS;
+Float_t boosted_bbtt_H_bb_m;
+
+// Declaration of leaf types for resolved tree
+Float_t resolved_bbtt_H_vis_tautau_pt_NOSYS;
+Float_t resolved_bbtt_H_vis_tautau_m;
+Float_t resolved_bbtt_H_bb_pt_NOSYS;
+Float_t resolved_bbtt_H_bb_m;
+
+// *************************************
+// Declaration of branches
+// *************************************
+
+// Declaration of branches for boosted tree
+TBranch *b_boosted_bbtt_H_vis_tautau_pt_NOSYS;
+TBranch *b_boosted_bbtt_H_vis_tautau_m;
+TBranch *b_boosted_bbtt_H_bb_pt_NOSYS;
+TBranch *b_boosted_bbtt_H_bb_m;
+
+// Declaration of branches for resolved tree
+TBranch *b_resolved_bbtt_H_vis_tautau_pt_NOSYS;
+TBranch *b_resolved_bbtt_H_vis_tautau_m;
+TBranch *b_resolved_bbtt_H_bb_pt_NOSYS;
+TBranch *b_resolved_bbtt_H_bb_m;
+
+
+// Declaration of histograms for boosted variables
+TH1F *hist_mH_tautau_boosted = new TH1F("hist_mH_tautau_boosted", "; m_{#tau #tau} [GeV];Events / bin", 100, 0, 500);
+TH1F *hist_mH_bb_boosted = new TH1F("hist_mH_bb_boosted", "$m_{bb}$ [GeV];Events / bin", 100, 0, 500);
+
+// Declaration of histograms for resolved variables
+TH1F *hist_mH_tautau_resolved = new TH1F("hist_mH_tautau_resolved", " ; m_{#tau #tau} [GeV];Events / bin", 100, 0, 500);
+
+// *************************************
+// Declaration of some functions
+// *************************************
+
+void plot_distributions(TTree* boosted_tree, TTree* resolved_tree);
+void set_branch_address(TTree* boosted_tree, TTree* resolved_tree);
 void print_list_of_branches(TTree* tree);
 void find_branches_names(TTree* tree, TString name_config, TString word);
 
 
-/*
-// This functions plots the pT, eta, phi, and other distributions for the H_bb and H_tautau and compare the distributions
+// This functions plots some distributions for the H_bb and H_tautau and compare the distributions
 // for the two configurations, boosted and resolved
 void plot_distributions(TTree* boosted_tree, TTree* resolved_tree){
 
+  Int_t b_entries = boosted_tree->GetEntries();
+  Int_t r_entries = resolved_tree->GetEntries();
+  int nbytes = 0;
+  
+  for(int ii=0; ii < b_entries; ii++){
 
+    nbytes = boosted_tree->GetEntry(ii);
+    hist_mH_tautau_boosted->Fill(boosted_bbtt_H_vis_tautau_m/1000.);
+  }
+
+  for(int ii=0; ii < r_entries; ii++){
+    
+    nbytes = resolved_tree->GetEntry(ii);
+    hist_mH_tautau_resolved->Fill(resolved_bbtt_H_vis_tautau_m/1000.);
+  }
+
+  TLegend *leg = new TLegend(0.55, 0.70, 0.90, 0.82);
+
+  //hist_mH_tautau_boosted->SetMarkerStyle(20);
+  //hist_mH_tautau_boosted->SetMarkerColor(kBlack);
+  hist_mH_tautau_boosted->SetStats(0);
+  hist_mH_tautau_boosted->SetFillStyle(3001);
+  hist_mH_tautau_boosted->SetFillColorAlpha(kBlue, 0.45);
+  hist_mH_tautau_boosted->SetLineColor(4);
+  hist_mH_tautau_boosted->SetTitle("Mass m_{#tau #tau} comparison between boosted and resolved processes");
+  
+  //hist_mH_tautau_resolved->SetMarkerStyle(20);
+  //hist_mH_tautau_resolved->SetMarkerColor(kBlack);
+  hist_mH_tautau_resolved->SetStats(0);
+  hist_mH_tautau_resolved->SetFillStyle(3003);
+  hist_mH_tautau_resolved->SetFillColorAlpha(kRed, 0.45);
+  hist_mH_tautau_resolved->SetLineColor(2);
+  hist_mH_tautau_resolved->SetTitle("Mass m_{#tau #tau} comparison between boosted and resolved processes");
+
+  ///// Plotting
+  TCanvas *can = new TCanvas("can","", 600, 800);
+
+  hist_mH_tautau_boosted->Draw("H");
+  hist_mH_tautau_resolved->Draw("sameH");
+  
+  leg->AddEntry(hist_mH_tautau_boosted, "m_{HH} for the boosted process", "l");
+  leg->AddEntry(hist_mH_tautau_resolved,"m_{HH} for the resolved process","l");
+  leg->SetBorderSize();
+  leg->Draw();
+  
+  can->Draw();
 }
-*/
+
+
+// This function saves the branches info for a given tree in the variables defined above
+void set_branch_address(TTree* boosted_tree, TTree* resolved_tree){
+
+  boosted_tree->SetBranchAddress("bbtt_H_vis_tautau_pt_NOSYS", &boosted_bbtt_H_vis_tautau_pt_NOSYS, &b_boosted_bbtt_H_vis_tautau_pt_NOSYS);
+  boosted_tree->SetBranchAddress("bbtt_H_vis_tautau_m", &boosted_bbtt_H_vis_tautau_m, &b_boosted_bbtt_H_vis_tautau_m);
+  boosted_tree->SetBranchAddress("bbtt_H_bb_pt_NOSYS", &boosted_bbtt_H_bb_pt_NOSYS, &b_boosted_bbtt_H_bb_pt_NOSYS);
+  boosted_tree->SetBranchAddress("bbtt_H_bb_m", &boosted_bbtt_H_bb_m, &b_boosted_bbtt_H_bb_m);
+  
+
+  resolved_tree->SetBranchAddress("bbtt_H_vis_tautau_pt_NOSYS", &resolved_bbtt_H_vis_tautau_pt_NOSYS, &b_resolved_bbtt_H_vis_tautau_pt_NOSYS);
+  resolved_tree->SetBranchAddress("bbtt_H_vis_tautau_m", &resolved_bbtt_H_vis_tautau_m, &b_resolved_bbtt_H_vis_tautau_m);
+  resolved_tree->SetBranchAddress("bbtt_H_bb_pt_NOSYS", &resolved_bbtt_H_bb_pt_NOSYS, &b_resolved_bbtt_H_bb_pt_NOSYS);
+  resolved_tree->SetBranchAddress("bbtt_H_bb_m", &resolved_bbtt_H_bb_m, &b_resolved_bbtt_H_bb_m);
+}
+
 
 // This function prints the list of branches of a given tree
 void print_list_of_branches(TTree* tree){
@@ -29,7 +133,7 @@ void print_list_of_branches(TTree* tree){
 // that contain some info related with some objects
 void find_branches_names(TTree* tree, TString name_config, TString word){
 
-  cout << "The list of branches that contains the word " << word << " for the " << name_config << " configuration are:"  << endl;
+  cout << "The list of branches that contain the word " << word << " for the " << name_config << " configuration are:"  << endl;
   
   for(int ii = 0; ii < tree->GetListOfBranches()->GetEntries(); ii++){
     
