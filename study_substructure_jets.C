7dd9bc771e53d95ff74751d708f0930c534eec9f
Miguel Ángel García Ruíz <miguel.angel.garcia.ruiz@cern.ch>
2024-04-02 16:01:07 +0200

    The plots for the distribution of the mass for the reconstructed jets for bb and tautau pairs are ploted

--- study_substructure_jets.C
+++ study_substructure_jets.C
@@ -1,4 +1,7 @@
-#include "study_substructure_jets.h"
+#include <TMath.h>
+#include <TLorentzVector.h>
+#include "declaration_of_functions.h"
+
 
 void study_substructure_jets(){
   
@@ -52,8 +55,52 @@ void study_substructure_jets(){
     compute_dR_min_index_fat_jets();
     //    truth_objects_asserts(ii);
     define_classes();
-    truth_matching_reco_objects_asserts(ii, count_b_tau_matched_recojets);
+    // truth_matching_reco_objects_asserts(ii, count_b_tau_matched_recojets);
 
+    if(class_event != -1){
+      
+      TLorentzVector jet1  = TLorentzVector();
+      TLorentzVector jet2  = TLorentzVector();
+      
+      // truth Boosted bb
+
+      if( (class_event == 2) || (class_event == 3) ){
+	hist_recojet_bb_m->Fill(recojet_antikt10UFO_m->at(idx_b1truth_recoak10_dRmin/1000.));
+      }
+      
+      // truth resolved bb
+      if( (class_event == 0) || (class_event == 1) ){
+
+	jet1.SetPtEtaPhiM(recojet_antikt10UFO_NOSYS_pt->at(idx_b1truth_recoak10_dRmin)/1000., recojet_antikt10UFO_eta->at(idx_b1truth_recoak10_dRmin), recojet_antikt10UFO_phi->at(idx_b1truth_recoak10_dRmin), recojet_antikt10UFO_m->at(idx_b1truth_recoak10_dRmin)/1000.);
+	
+	jet2.SetPtEtaPhiM(recojet_antikt10UFO_NOSYS_pt->at(idx_b2truth_recoak10_dRmin)/1000., recojet_antikt10UFO_eta->at(idx_b2truth_recoak10_dRmin), recojet_antikt10UFO_phi->at(idx_b2truth_recoak10_dRmin), recojet_antikt10UFO_m->at(idx_b2truth_recoak10_dRmin)/1000.);
+
+	TLorentzVector sum_jet = jet1 + jet2;
+	
+	hist_recojet_bb_m->Fill(sum_jet.Mag());
+      }
+
+      // truth boosted tautau jets
+
+      if( (class_event == 1) || (class_event == 3) ){
+	hist_recojet_tautau_m->Fill(recojet_antikt10UFO_m->at(idx_tau1truth_recoak10_dRmin/1000.));
+      }
+      
+      // truth resolved tautau jets
+      if( (class_event == 0) || (class_event == 2) ){
+
+	jet1.SetPtEtaPhiM(recojet_antikt10UFO_NOSYS_pt->at(idx_tau1truth_recoak10_dRmin)/1000., recojet_antikt10UFO_eta->at(idx_tau1truth_recoak10_dRmin), recojet_antikt10UFO_phi->at(idx_tau1truth_recoak10_dRmin), recojet_antikt10UFO_m->at(idx_tau1truth_recoak10_dRmin)/1000.);
+	
+	jet2.SetPtEtaPhiM(recojet_antikt10UFO_NOSYS_pt->at(idx_tau2truth_recoak10_dRmin)/1000., recojet_antikt10UFO_eta->at(idx_tau2truth_recoak10_dRmin), recojet_antikt10UFO_phi->at(idx_tau2truth_recoak10_dRmin), recojet_antikt10UFO_m->at(idx_tau2truth_recoak10_dRmin)/1000.);
+
+	TLorentzVector sum_jet = jet1 + jet2;
+	
+	hist_recojet_tautau_m->Fill(sum_jet.Mag());
+      }
+      
+      
+    }
+    
     if(class_event == -1){ count_non_matched_events+=1; }
     if(class_event == 0){ count_truth_Rbb_Rtautau+=1; } 
     if(class_event == 1){ count_truth_Rbb_Btautau+=1; }
@@ -71,7 +118,7 @@ void study_substructure_jets(){
 
   cout << "There are " << count_truth_Rbb_Rtautau << " in the truth R_bb-Rtautau (" << 100.0*count_truth_Rbb_Rtautau/nentries << "% of the total entries)" << endl;
   cout << "There are " << count_truth_Rbb_Btautau << " in the truth R_bb-Btautau (" << 100.0*count_truth_Rbb_Btautau/nentries << "% of the total entries)" << endl;
-  cout << "There are " << count_truth_Bbb_Rtautau << " in the truth R_bb-Rtautau (" << 100.0*count_truth_Bbb_Rtautau/nentries << "% of the total entries)" << endl;
+  cout << "There are " << count_truth_Bbb_Rtautau << " in the truth B_bb-Rtautau (" << 100.0*count_truth_Bbb_Rtautau/nentries << "% of the total entries)" << endl;
   cout << "There are " << count_truth_Bbb_Btautau << " in the truth B_bb-Btautau (" << 100.0*count_truth_Bbb_Btautau/nentries << "% of the total entries)" << endl;
 
   cout << "There are " << count_fake_Rbb_Rtautau << " in the fake R_bb-Rtautau (" << 100.0*count_fake_Rbb_Rtautau/nentries << "% of the total entries)" << endl;
@@ -79,6 +126,7 @@ void study_substructure_jets(){
   cout << "There are " << count_fake_Bbb_Rtautau << " in the fake B_bb-Rtautau (" << 100.0*count_fake_Bbb_Rtautau/nentries << "% of the total entries)" << endl;
   cout << "There are " << count_fake_Bbb_Btautau << " in the fake B_bb-Btautau (" << 100.0*count_fake_Bbb_Btautau/nentries << "% of the total entries)" << endl;
 
+  
   int sum_all_events = count_non_matched_events + count_truth_Rbb_Rtautau + count_truth_Rbb_Btautau + count_truth_Bbb_Rtautau + count_truth_Bbb_Btautau + count_fake_Rbb_Rtautau + count_fake_Rbb_Btautau + count_fake_Bbb_Rtautau + count_fake_Bbb_Btautau;
 
   int sum_truth_matching_events = count_truth_Rbb_Rtautau + count_truth_Rbb_Btautau + count_truth_Bbb_Rtautau + count_truth_Bbb_Btautau;
@@ -94,7 +142,11 @@ void study_substructure_jets(){
   cout << "The sum of all the events which were wrongly classified with resolved and boosted bb and tautau jets: " << sum_fake_matching_events << " (" << 100.0*sum_fake_matching_events/nentries << "% of the total entries)" << endl;
 
   cout << "Total entries: " << nentries << endl;
-  
+
+
+  plot_distributions("recojet_bb_m");
+  plot_distributions("recojet_tautau_m");
+
   inFile->Close();
   
 }
