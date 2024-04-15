a586827067e74a171b28fe5d6d769b31ebcf2c4f
Miguel Ángel García Ruíz <miguel.angel.garcia.ruiz@cern.ch>
2024-04-15 15:29:10 +0200

    The analysis here is given by the implementation of cuts in the mass of the HH to study the overlaping between the boosted events (for the four classes) and the events passing the Resolved selection

---
+++ variables_cuts_implementation.h
@@ -0,0 +1,216 @@
+// *************************************
+// Reading the input files
+// *************************************
+
+// Path of the output sample for the study of the substructure part                                                                                                               
+TString path_sample="output/study_boosted_regions.root";
+
+// Open the input files and access to the trees                                                                                             
+
+TFile* outFile = TFile::Open(path_sample);
+TTree* outTree = (TTree*) outFile->Get("AnalysisMiniTree");
+
+
+// *************************************
+// Declaration of leaf variables
+// *************************************
+
+
+// Declaration of leaf types for truth_children branches for boosted tree variables
+
+// truth children from Higgs
+vector<int> *truth_children_fromH1_pdgId;
+vector<float> *truth_children_fromH1_pt;
+vector<float> *truth_children_fromH1_eta;
+vector<float> *truth_children_fromH1_phi;
+vector<float> *truth_children_fromH1_m;
+
+vector<int> *truth_children_fromH2_pdgId;
+vector<float> *truth_children_fromH2_pt;
+vector<float> *truth_children_fromH2_eta;
+vector<float> *truth_children_fromH2_phi;
+vector<float> *truth_children_fromH2_m;
+
+// Truth HH variables
+Float_t truth_HH_pt;
+Float_t truth_HH_eta;
+Float_t truth_HH_phi;
+Float_t truth_HH_m;
+
+// Matching of truth b and tau jets from truth children
+Float_t truth_b1_pt;
+Float_t truth_b1_eta;
+Float_t truth_b1_phi;
+Float_t truth_b1_m;
+
+Float_t truth_b2_pt;
+Float_t truth_b2_eta;
+Float_t truth_b2_phi;
+Float_t truth_b2_m;
+
+Float_t truth_tau1_pt;
+Float_t truth_tau1_eta;
+Float_t truth_tau1_phi;
+Float_t truth_tau1_m;
+
+Float_t truth_tau2_pt;
+Float_t truth_tau2_eta;
+Float_t truth_tau2_phi;
+Float_t truth_tau2_m;
+
+
+// Declaration of leafs types for reconstructed_ak10UFO jets (fat-jets) branches for boosted tree variables
+vector<float> *recojet_antikt10UFO_NOSYS_pt;
+vector<float> *recojet_antikt10UFO_eta;
+vector<float> *recojet_antikt10UFO_phi;
+vector<float> *recojet_antikt10UFO_m;
+
+// Declaration idx and dRmin values
+
+Int_t idx_b1truth_recoak10_dRmin;
+Int_t idx_b2truth_recoak10_dRmin;
+Int_t idx_tau1truth_recoak10_dRmin;
+Int_t idx_tau2truth_recoak10_dRmin;
+
+Float_t dR_min_b1truth_recoak10_fatjet;
+Float_t dR_min_b2truth_recoak10_fatjet;
+Float_t dR_min_tau1truth_recoak10_fatjet;
+Float_t dR_min_tau2truth_recoak10_fatjet;
+
+int class_event;
+
+// Declaration of the resolved variables results
+
+Float_t bbtt_Jet_b1_pt_NOSYS;
+Float_t bbtt_Jet_b1_eta;
+Float_t bbtt_Jet_b1_phi;
+Float_t bbtt_Jet_b1_E;
+
+Float_t bbtt_Jet_b2_pt_NOSYS;
+Float_t bbtt_Jet_b2_eta;
+Float_t bbtt_Jet_b2_phi;
+Float_t bbtt_Jet_b2_E;
+
+Float_t bbtt_Tau1_pt_NOSYS;
+Float_t bbtt_Tau1_eta;
+Float_t bbtt_Tau1_phi;
+Float_t bbtt_Tau1_E;
+
+Float_t bbtt_Tau2_pt_NOSYS;
+Float_t bbtt_Tau2_eta;
+Float_t bbtt_Tau2_phi;
+Float_t bbtt_Tau2_E;
+
+Float_t bbtt_HH_pt_NOSYS;
+Float_t bbtt_HH_eta;
+Float_t bbtt_HH_phi;
+Float_t bbtt_HH_m;
+
+
+// *************************************
+// Declaration of branches
+// *************************************
+
+
+// truth childre from Higgs
+TBranch *b_truth_children_fromH1_pdgId;
+TBranch *b_truth_children_fromH1_pt;
+TBranch *b_truth_children_fromH1_eta;
+TBranch *b_truth_children_fromH1_phi;
+TBranch *b_truth_children_fromH1_m;
+
+TBranch *b_truth_children_fromH2_pdgId;
+TBranch *b_truth_children_fromH2_pt;
+TBranch *b_truth_children_fromH2_eta;
+TBranch *b_truth_children_fromH2_phi;
+TBranch *b_truth_children_fromH2_m;
+
+// Truth HH variables
+TBranch *b_truth_HH_pt;
+TBranch *b_truth_HH_eta;
+TBranch *b_truth_HH_phi;
+TBranch *b_truth_HH_m;
+
+// Matching of truth b and tau jets from truth children
+TBranch *b_truth_b1_pt;
+TBranch *b_truth_b1_eta;
+TBranch *b_truth_b1_phi;
+TBranch *b_truth_b1_m;
+
+TBranch *b_truth_b2_pt;
+TBranch *b_truth_b2_eta;
+TBranch *b_truth_b2_phi;
+TBranch *b_truth_b2_m;
+
+TBranch *b_truth_tau1_pt;
+TBranch *b_truth_tau1_eta;
+TBranch *b_truth_tau1_phi;
+TBranch *b_truth_tau1_m;
+
+TBranch *b_truth_tau2_pt;
+TBranch *b_truth_tau2_eta;
+TBranch *b_truth_tau2_phi;
+TBranch *b_truth_tau2_m;
+
+// Declaration branches for reconstructed_ak10UFO jets (fat-jets) branches for boosted tree variables
+TBranch *b_recojet_antikt10UFO_NOSYS_pt;
+TBranch *b_recojet_antikt10UFO_eta;
+TBranch *b_recojet_antikt10UFO_phi;
+TBranch *b_recojet_antikt10UFO_m;
+
+// Declaration idx and dRmin values
+
+TBranch *b_idx_b1truth_recoak10_dRmin;
+TBranch *b_idx_b2truth_recoak10_dRmin;
+TBranch *b_idx_tau1truth_recoak10_dRmin;
+TBranch *b_idx_tau2truth_recoak10_dRmin;
+
+TBranch *b_dR_min_b1truth_recoak10_fatjet;
+TBranch *b_dR_min_b2truth_recoak10_fatjet;
+TBranch *b_dR_min_tau1truth_recoak10_fatjet;
+TBranch *b_dR_min_tau2truth_recoak10_fatjet;
+
+TBranch *b_class_event;
+
+// Declaration of the resolved variables results
+
+TBranch *b_bbtt_Jet_b1_pt_NOSYS;
+TBranch *b_bbtt_Jet_b1_eta;
+TBranch *b_bbtt_Jet_b1_phi;
+TBranch *b_bbtt_Jet_b1_E;
+
+TBranch *b_bbtt_Jet_b2_pt_NOSYS;
+TBranch *b_bbtt_Jet_b2_eta;
+TBranch *b_bbtt_Jet_b2_phi;
+TBranch *b_bbtt_Jet_b2_E;
+
+TBranch *b_bbtt_Tau1_pt_NOSYS;
+TBranch *b_bbtt_Tau1_eta;
+TBranch *b_bbtt_Tau1_phi;
+TBranch *b_bbtt_Tau1_E;
+
+TBranch *b_bbtt_Tau2_pt_NOSYS;
+TBranch *b_bbtt_Tau2_eta;
+TBranch *b_bbtt_Tau2_phi;
+TBranch *b_bbtt_Tau2_E;
+
+TBranch *b_bbtt_HH_pt_NOSYS;
+TBranch *b_bbtt_HH_eta;
+TBranch *b_bbtt_HH_phi;
+TBranch *b_bbtt_HH_m;
+
+// *************************************
+// Declaration of histograms
+// *************************************
+
+// Histograms for mass of HH cuts ratio_1
+
+TGraph *graph_mHH_cut_r1_class0 = new TGraph();
+TGraph *graph_mHH_cut_r1_class1 = new TGraph();
+TGraph *graph_mHH_cut_r1_class2 = new TGraph();
+TGraph *graph_mHH_cut_r1_class3 = new TGraph();
+
+TGraph *graph_mHH_cut_r2_class0 = new TGraph();
+TGraph *graph_mHH_cut_r2_class1 = new TGraph();
+TGraph *graph_mHH_cut_r2_class2 = new TGraph();
+TGraph *graph_mHH_cut_r2_class3 = new TGraph();
