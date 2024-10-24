d164bcb3f7651f0ec3a82a440375d3c38059007b
garciarm <miguel.angel.garcia.ruiz@cern.ch>
2024-10-24 09:24:56 +0200

    The unordered map to save the TEfficiency plots is added. Now the correct order of samples names and TEff objects should match well

--- Plotting/Plotting_aceptance_ratios.C
+++ Plotting/Plotting_aceptance_ratios.C
@@ -6,6 +6,17 @@ void Plotting_aceptance_ratios(string nameVar, string min_pT){
   std::vector<string> sample_files = {"ggF_lambda10_hh_600460", "ggF_lambda10_lh_600462", "ggF_SM_hh_600459", "ggF_SM_lh_600461",
     "VBF_cvv1p5_hh_502985", "VBF_cvv1p5_lh_502996", "VBF_SM_hh_502982", "VBF_SM_lh_502993"};
 
+  // Define the unordered map
+  std::unordered_map<std::string, std::vector<TEfficiency>> efficiency_map;
+
+  initializeMapRatiosInfo(sample_files, nameVar, min_pT, efficiency_map);
+
+  plotEfficiencies(sample_files, "r1" , nameVar, min_pT, efficiency_map);
+  plotEfficiencies(sample_files, "r2" , nameVar, min_pT, efficiency_map);
+  plotEfficiencies(sample_files, "r3" , nameVar, min_pT, efficiency_map);
+  plotEfficiencies(sample_files, "r4" , nameVar, min_pT, efficiency_map);
+    
+  /*
   std::vector<TEfficiency*> TEff_ratios_r1;
   std::vector<TEfficiency*> TEff_ratios_r2;
   std::vector<TEfficiency*> TEff_ratios_r3;
@@ -17,5 +28,6 @@ void Plotting_aceptance_ratios(string nameVar, string min_pT){
   plotEfficiencies(sample_files, "r2", nameVar, min_pT, TEff_ratios_r2);
   plotEfficiencies(sample_files, "r3", nameVar, min_pT, TEff_ratios_r3);
   plotEfficiencies(sample_files, "r4", nameVar, min_pT, TEff_ratios_r4);
-
+  */
+  
 }
--- Plotting/Plotting_aceptance_ratios.h
+++ Plotting/Plotting_aceptance_ratios.h
@@ -3,6 +3,7 @@
 #include "AtlasStyle/AtlasLabels.C"
 #include <unordered_map>
 
+
 struct hist_ratios {
   TH1F* hist_num_for_r1;
   TH1F* hist_den_for_r1;
@@ -25,15 +26,16 @@ struct plot_Teff {
 // Declaration of functions
 //*******************************************************
 
-void plotEfficiencies(const std::vector<std::string>& sampleFiles, const std::string& ratio, const std::string& nameVar, const std::string& min_pT, const std::vector<TEfficiency*>& TEff_ratios);
-
-void initializeMapRatiosInfo(const std::vector<std::string>& sampleFiles, const std::string& nameVar, const std::string& min_pT, std::vector<TEfficiency*>& TEff_ratios_r1, std::vector<TEfficiency*>& TEff_ratios_r2, std::vector<TEfficiency*>& TEff_ratios_r3, std::vector<TEfficiency*>& TEff_ratios_r4);
-
+void plotEfficiencies(const std::vector<std::string>& sampleFiles, const std::string& ratio, const std::string& nameVar, const std::string& min_pT, const std::unordered_map<std::string, std::vector<TEfficiency>>& TEff_ratios);
+  
+void initializeMapRatiosInfo(const std::vector<std::string>& sampleFiles, const std::string& nameVar, const std::string& min_pT,
+			     std::unordered_map<std::string, std::vector<TEfficiency>>& efficiency_map);
+  
 //*******************************************************
 // Definition of functions declared above
 //*******************************************************
 
-void plotEfficiencies(const std::vector<std::string>& sampleFiles, const std::string& ratio, const std::string& nameVar, const std::string& min_pT, const std::vector<TEfficiency*>& TEff_ratios){
+void plotEfficiencies(const std::vector<std::string>& sampleFiles, const std::string& ratio, const std::string& nameVar, const std::string& min_pT, const std::unordered_map<std::string, std::vector<TEfficiency>>& TEff_ratios){
 
   gROOT->SetBatch(kTRUE);
   SetAtlasStyle();
@@ -41,41 +43,108 @@ void plotEfficiencies(const std::vector<std::string>& sampleFiles, const std::st
   TCanvas* canvas = new TCanvas(("can_"+nameVar+"_minpT"+min_pT+ratio).c_str());
   TLegend* leg = new TLegend(0.18, 0.75, 0.93, 0.90);
 
-  const int colors[8] = { kRed, kBlue, kGreen, kMagenta, kCyan, kYellow, kBlack, kOrange };
+  //TColor colors[8] = { kRed, kBlue, kGreen, kMagenta, kCyan, kYellow, kBlack, kOrange };
 
-  for(int ii=0; ii<8; ii++){
-    TEff_ratios.at(ii)->SetMarkerStyle(20);
-    TEff_ratios.at(ii)->SetMarkerColor(colors[ii]);
-    leg->AddEntry(TEff_ratios.at(ii), (sampleFiles[ii]+"_"+ratio).c_str(), "lep");
-    if(ii==0){
-      TEff_ratios.at(ii)->Draw("AP");
-    }
-    else{
-      TEff_ratios.at(ii)->Draw("PSAME");
-    }
+  int r_plot = 0;
+  int color_number = 0;
+
+  if(ratio.find("r1")!=std::string::npos){ r_plot = 0;}
+  if(ratio.find("r2")!=std::string::npos){ r_plot = 1;}
+  if(ratio.find("r3")!=std::string::npos){ r_plot = 2;}
+  if(ratio.find("r4")!=std::string::npos){ r_plot = 3;}
+  
+  TEfficiency hist_ggF_lambda10_hh_600460;
+  TEfficiency hist_ggF_lambda10_lh_600462;
+  TEfficiency hist_ggF_SM_hh_600459;
+  TEfficiency hist_ggF_SM_lh_600461;
+  TEfficiency hist_VBF_cvv1p5_hh_502985;
+  TEfficiency hist_VBF_cvv1p5_lh_502996;
+  TEfficiency hist_VBF_SM_hh_502982;
+  TEfficiency hist_VBF_SM_lh_502993;
+    
+  for(const auto& process_sample : TEff_ratios) {
+
+    const std::string& sample_name = process_sample.first;
+    const auto& ratios = process_sample.second;
+    
+    // Make a copy of the TEfficiency object to modify it
+    TEfficiency ratio_hist = ratios[r_plot]; // Copy the TEfficiency object
+
+    //cout << "plot for: "+sample_name << endl;
+
+    ratio_hist.SetMarkerStyle(20);
+    //ratio_hist.SetMarkerColor(colors[color_number]);
+    
+    //ratio_hist.Draw();
+    //string name_image = "output_combined_ratios_plots/"+min_pT+"GeV/"+nameVar+"_min_pT"+min_pT+"_ratio_r1_"+sample_name+".png";
+    //canvas->SaveAs(name_image.c_str());
+
+    if(sample_name.find("ggF_lambda10_hh_600460")!=std::string::npos){ hist_ggF_lambda10_hh_600460 = ratio_hist; }
+    if(sample_name.find("ggF_lambda10_lh_600462")!=std::string::npos){ hist_ggF_lambda10_lh_600462 = ratio_hist; }
+    if(sample_name.find("ggF_SM_hh_600459")!=std::string::npos){ hist_ggF_SM_hh_600459 = ratio_hist; }
+    if(sample_name.find("ggF_SM_lh_600461")!=std::string::npos){ hist_ggF_SM_lh_600461 = ratio_hist; }
+    if(sample_name.find("VBF_cvv1p5_hh_502985")!=std::string::npos){ hist_VBF_cvv1p5_hh_502985 = ratio_hist; }
+    if(sample_name.find("VBF_cvv1p5_lh_502996")!=std::string::npos){ hist_VBF_cvv1p5_lh_502996 = ratio_hist; }
+    if(sample_name.find("VBF_SM_hh_502982")!=std::string::npos){ hist_VBF_SM_hh_502982 = ratio_hist; }
+    if(sample_name.find("VBF_SM_lh_502993")!=std::string::npos){ hist_VBF_SM_lh_502993 = ratio_hist; }
+
   }
-      
+
+  hist_ggF_lambda10_hh_600460.SetMarkerColor(kRed);
+  hist_ggF_lambda10_lh_600462.SetMarkerColor(kBlue);
+  hist_ggF_SM_hh_600459.SetMarkerColor(kGreen);
+  hist_ggF_SM_lh_600461.SetMarkerColor(kMagenta);
+  hist_VBF_cvv1p5_hh_502985.SetMarkerColor(kCyan);
+  hist_VBF_cvv1p5_lh_502996.SetMarkerColor(kYellow);
+  hist_VBF_SM_hh_502982.SetMarkerColor(kBlack);
+  hist_VBF_SM_lh_502993.SetMarkerColor(kOrange);
+
+  leg->AddEntry(&hist_ggF_lambda10_hh_600460, ("ggF_lambda10_hh_600460_"+ratio).c_str(), "lep");
+  leg->AddEntry(&hist_ggF_lambda10_lh_600462, ("ggF_lambda10_lh_600462_"+ratio).c_str(), "lep");
+  leg->AddEntry(&hist_ggF_SM_hh_600459, ("ggF_SM_hh_600459_"+ratio).c_str(), "lep");
+  leg->AddEntry(&hist_ggF_SM_lh_600461, ("ggF_SM_lh_600461_"+ratio).c_str(), "lep");
+  leg->AddEntry(&hist_VBF_cvv1p5_hh_502985, ("VBF_cvv1p5_hh_502985_"+ratio).c_str(), "lep");
+  leg->AddEntry(&hist_VBF_cvv1p5_lh_502996, ("VBF_cvv1p5_lh_502996_"+ratio).c_str(), "lep");
+  leg->AddEntry(&hist_VBF_SM_hh_502982, ("VBF_SM_hh_502982_"+ratio).c_str(), "lep");
+  leg->AddEntry(&hist_VBF_SM_lh_502993, ("VBF_SM_lh_502993_"+ratio).c_str(), "lep");
+
+  
+  hist_ggF_lambda10_hh_600460.Draw("AP");
+  hist_ggF_lambda10_lh_600462.Draw("PSAME");
+  hist_ggF_SM_hh_600459.Draw("PSAME");
+  hist_ggF_SM_lh_600461.Draw("PSAME");
+  hist_VBF_cvv1p5_hh_502985.Draw("PSAME");
+  hist_VBF_cvv1p5_lh_502996.Draw("PSAME");
+  hist_VBF_SM_hh_502982.Draw("PSAME");
+  hist_VBF_SM_lh_502993.Draw("PSAME");
+
+
   gPad->Update();
-  auto graph = TEff_ratios.at(0)->GetPaintedGraph();
+  auto graph = hist_ggF_lambda10_hh_600460.GetPaintedGraph();
   graph->SetMinimum(0);
   graph->SetMaximum(1.40);
-      
+  
   gPad->Update();
+  
   leg->SetNColumns(3);
   leg->SetFillStyle(0);
   leg->SetLineColor(0);
   
   leg->Draw();
-
+  
   string name_image = "output_combined_ratios_plots/"+min_pT+"GeV/"+nameVar+"_min_pT"+min_pT+"_ratios_"+ratio+"_comparison.png";
   canvas->SaveAs(name_image.c_str());
+  
 }
 
 // Initialize the ap_ratios_info
-void initializeMapRatiosInfo(const std::vector<std::string>& sampleFiles, const std::string& nameVar, const std::string& min_pT, std::vector<TEfficiency*>& TEff_ratios_r1, std::vector<TEfficiency*>& TEff_ratios_r2, std::vector<TEfficiency*>& TEff_ratios_r3, std::vector<TEfficiency*>& TEff_ratios_r4){
+void initializeMapRatiosInfo(const std::vector<std::string>& sampleFiles, const std::string& nameVar, const std::string& min_pT,
+			     std::unordered_map<std::string, std::vector<TEfficiency>>& efficiency_map){
   
   string path_folder="/eos/user/g/garciarm/HHbbtautau-easyjet-framework-analysis/boosted-analysis/Analysis/study_substructure_jets/output_analysis/";
-   
+
+  gROOT->SetBatch(kTRUE);
+  
   for (const auto& sample : sampleFiles){
     string path_root_file = path_folder+sample+"_pT"+min_pT+"GeV.root";
     TCanvas* can = new TCanvas(("can_"+sample+"_"+nameVar+"_pT"+min_pT+"GeV").c_str());
@@ -90,15 +159,6 @@ void initializeMapRatiosInfo(const std::vector<std::string>& sampleFiles, const 
     else{
       cout << "The file has been read " << sample << endl;
     }
-    /*
-    TH1F* hist_num_r1_r2 = dynamic_cast<TH1F*>(file->Get(("hist_acceptance_"+nameVar+"_numerator_class3_r1_r2").c_str()));
-    TH1F* hist_den_for_r1 = dynamic_cast<TH1F*>(file->Get(("hist_acceptance_"+nameVar+"_denominator_r1").c_str()));
-    TH1F* hist_den_for_r2 = dynamic_cast<TH1F*>(file->Get(("hist_acceptance_"+nameVar+"_denominator_class3_r2").c_str()));
-
-    TH1F* hist_num_for_r3 = dynamic_cast<TH1F*>(file->Get(("hist_acceptance_"+nameVar+"_numerator_r3").c_str()));
-    TH1F* hist_num_for_r4 = dynamic_cast<TH1F*>(file->Get(("hist_acceptance_"+nameVar+"_numerator_class3_r4").c_str()));
-    TH1F* hist_den_for_r3_r4 = dynamic_cast<TH1F*>(file->Get(("hist_acceptance_"+nameVar+"_denominator_r3_r4").c_str()));
-    */
 
     string name_hist_num_r1_r2 = ""; string name_hist_den_for_r1 = ""; string name_hist_den_for_r2 = "";
     string name_hist_num_for_r3 = ""; string name_hist_num_for_r4 = ""; string name_hist_den_for_r3_r4 = ""; 
@@ -128,8 +188,7 @@ void initializeMapRatiosInfo(const std::vector<std::string>& sampleFiles, const 
       name_hist_num_for_r4 = "hist_acceptance_all_Bbb_"+nameVar+"_denominator_r2_numerator_r4";
       name_hist_den_for_r3_r4 = "hist_acceptance_"+nameVar+"_denominator_r3_r4";
     }
-   
-    
+
     TH1F* hist_num_r1_r2 = dynamic_cast<TH1F*>(file->Get(name_hist_num_r1_r2.c_str()));
     TH1F* hist_den_for_r1 = dynamic_cast<TH1F*>(file->Get(name_hist_den_for_r1.c_str()));
     TH1F* hist_den_for_r2 = dynamic_cast<TH1F*>(file->Get(name_hist_den_for_r2.c_str()));
@@ -138,42 +197,31 @@ void initializeMapRatiosInfo(const std::vector<std::string>& sampleFiles, const 
     TH1F* hist_num_for_r4 = dynamic_cast<TH1F*>(file->Get(name_hist_num_for_r4.c_str()));
     TH1F* hist_den_for_r3_r4 = dynamic_cast<TH1F*>(file->Get(name_hist_den_for_r3_r4.c_str()));
 
-    
-    //********************************************************************
-    //Plotting numerator histograms r1_r2
-    //********************************************************************
+    cout << "/----------------------------------------------------------------------" << endl;
+    cout << "For: "+nameVar << endl;
+    cout << "\t For: "+min_pT << endl;
+    cout << "\t \t name process: "+sample << "\t r1_acceptance (%): " << (hist_num_r1_r2->Integral()/hist_den_for_r1->Integral())*100.0 << endl;
+    cout << "\t \t name process: "+sample << "\t r2_acceptance (%): " << (hist_num_r1_r2->Integral()/hist_den_for_r2->Integral())*100.0 << endl;
+    cout << "\t \t name process: "+sample << "\t r3_acceptance (%): " << (hist_num_for_r3->Integral()/hist_den_for_r3_r4->Integral())*100.0 << endl;
+    cout << "\t \t name process: "+sample << "\t r4_acceptance (%): " << (hist_num_for_r4->Integral()/hist_den_for_r3_r4->Integral())*100.0 << endl;
+    cout << "/----------------------------------------------------------------------" << endl;
 
-    gROOT->SetBatch(kTRUE);
-    hist_num_r1_r2->Draw("H");
-    string name_image = "output_combined_ratios_plots/"+min_pT+"GeV/"+nameVar+"_min_pT"+min_pT+"_num_ratios_r1_r2_"+sample+".png";
-    can->SaveAs(name_image.c_str());
-
-    //********************************************************************
-    //Plotting numerator histograms r3
-    //********************************************************************
     
-    hist_num_for_r3->Draw("H");
-    name_image = "output_combined_ratios_plots/"+min_pT+"GeV/"+nameVar+"_min_pT"+min_pT+"_num_ratios_r3_"+sample+".png";
-    can->SaveAs(name_image.c_str());
-
-    //********************************************************************
-    //Plotting numerator histograms r4
-    //********************************************************************
-    
-    hist_num_for_r4->Draw("H");
-    name_image = "output_combined_ratios_plots/"+min_pT+"GeV/"+nameVar+"_min_pT"+min_pT+"_num_ratios_r4_"+sample+".png";
-    can->SaveAs(name_image.c_str());
-   
     TEfficiency *pEff_r1 = new TEfficiency(*hist_num_r1_r2, *hist_den_for_r1);
     TEfficiency *pEff_r2 = new TEfficiency(*hist_num_r1_r2, *hist_den_for_r2);
     TEfficiency *pEff_r3 = new TEfficiency(*hist_num_for_r3, *hist_den_for_r3_r4);
     TEfficiency *pEff_r4 = new TEfficiency(*hist_num_for_r4, *hist_den_for_r3_r4);
 
-    TEff_ratios_r1.push_back(pEff_r1);
-    TEff_ratios_r2.push_back(pEff_r2);
-    TEff_ratios_r3.push_back(pEff_r3);
-    TEff_ratios_r4.push_back(pEff_r4);
+    //pEff_r1->Draw();
+    // string name_image = "output_combined_ratios_plots/"+min_pT+"GeV/"+nameVar+"_min_pT"+min_pT+"_ratio_r1_"+sample+".png";
+    //can->SaveAs(name_image.c_str());
     
+    // Store the ratios in a vector
+    std::vector<TEfficiency> list_TEff_ratios = {*pEff_r1, *pEff_r2, *pEff_r3, *pEff_r4};
+
+    // Insert into the map: for the given process, add the list of TEfficiency
+    efficiency_map[sample] = list_TEff_ratios;
+   
     file->Close();
   }
 }
