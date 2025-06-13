7ad34280a8eab0339faf2db388e2cd7c709e1354
garciarm <miguel.angel.garcia.ruiz@cern.ch>
2025-06-13 17:54:17 +0200

    The python files for the BDT model is modularized

--- Analysis/VBF_ggF_separation/BDT_model_h5.py
+++ Analysis/VBF_ggF_separation/BDT_model_h5.py
@@ -15,10 +15,9 @@ from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.metrics import roc_curve, auc
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
-# from skl2onnx.helpers import save_model
-# from skl2onnx.helpers
 import onnx
 from onnx import helper, numpy_helper, TensorProto
+import plotting_BDT
 
 # Reading VBF File
 # file_VBF = "HHARD_input_h5files/mva_VBFHHbbttSM_ONE_PASS.h5"
@@ -70,12 +69,7 @@ y = pd.concat([y_ggF, y_VBF]).astype(int)
 # print(X)
 # print(y)
 
-correlation_matrix = X.corr()
-plt.figure(figsize=(20, 16)) # make new figure
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", cbar_kws={"shrink": 0.8})
-plt.title("Variables correlation matrix")
-plt.tight_layout() 
-plt.savefig("BDT_plots/correlation_matrix.pdf")
+plotting_BDT.correlation_plots(X) # plotting correlation between variables
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=492)
 
@@ -105,15 +99,7 @@ print("Ranking of the most important variables:")
 for i in range(len(features)):
     print(f"{i+1}. {features[indices[i]]} ({importances[indices[i]]:.3f})")
 
-# Plotting                                                                                                                                                     
-plt.figure(figsize=(8, 6))
-plt.title("Importance of each variable")
-plt.bar(range(len(features)), importances[indices], align="center")
-plt.xticks(range(len(features)), features[indices], rotation=90)
-plt.tight_layout()
-plt.savefig("BDT_plots/importance_ranking_BDT.pdf")
-plt.figure()
-#plt.show()
+plotting_BDT.ranking_variables_plot(features, importances, indices) # plotting ranking of variables
 
 y_predicted = bdt.predict(X_test) # get predicted y for test set
 print (classification_report(y_test, y_predicted,
@@ -121,115 +107,9 @@ print (classification_report(y_test, y_predicted,
 print ("Area under ROC curve for test data: %.4f"%(roc_auc_score(y_test,
                                                     bdt.decision_function(X_test))) )
 
-# we first plot the Neural Network output
-# signal_decisions = bdt.decision_function(X[y>0.5]).ravel() # get probabilities on signal
-# background_decisions = bdt.decision_function(X[y<0.5]).ravel() # get decisions on background
+plotting_BDT.roc_curve_plots(bdt, X, y, X_test, y_test) # plotting roc curve for the bdt
 
-signal_decisions = bdt.predict_proba(X[y>0.5])[:, 1] # get probabilities on signal
-background_decisions = bdt.predict_proba(X[y<0.5])[:, 1] # get decisions on background
-
-plt.hist(background_decisions, color='red', label='ggF processes', 
-         histtype='step', # lineplot that's unfilled
-         density=True ) # normalize to form a probability density
-plt.hist(signal_decisions, color='blue', label='VBF processes', 
-         histtype='step', # lineplot that's unfilled
-         density=True, # normalize to form a probability density
-         linestyle='--' ) # dashed line
-plt.xlabel('BDT output') # add x-axis label
-plt.ylabel('Arbitrary units') # add y-axis label
-plt.legend() # add legend
-#plt.show()
-plt.savefig("BDT_plots/distributions_BDT_score.pdf")
-
-# we then plot the ROC
-plt.figure() # make new figure 
-
-decisions = bdt.decision_function(X_test).ravel() # get probabilities on test set
-
-# Compute ROC curve and area under the curve
-fpr, tpr, _ = roc_curve(y_test, # actual
-                        decisions ) # predicted
-
-# Compute area under the curve for training set
-roc_auc = auc(fpr, # false positive rate 
-              tpr) # true positive rate
-
-plt.plot(fpr, tpr, label='ROC (area = %0.2f)'%(roc_auc)) # plot test ROC curve
-plt.plot([0, 1], # x from 0 to 1
-         [0, 1], # y from 0 to 1
-         '--', # dashed line
-         color='grey', label='Luck')
-
-plt.xlabel('False Positive Rate') # x-axis label
-plt.ylabel('True Positive Rate') # y-axis label
-plt.title('Receiver operating characteristic (ROC) curve') # title
-plt.legend() # add legend
-plt.grid() # add grid
-#plt.show()
-plt.savefig("BDT_plots/ROC_Curve.pdf")
-
-### BDT Overtraining check
-
-def compare_train_test(clf, X_train, y_train, X_test, y_test):
-    decisions = [] # list to hold decisions of classifier
-    for X,y in ((X_train, y_train), (X_test, y_test)): # train and test
-        #d1 = clf.decision_function(X[y<0.5]).ravel() # background
-        #d2 = clf.decision_function(X[y>0.5]).ravel() # signal
-        d1 = clf.predict_proba(X[y<0.5])[:, 1] # background
-        d2 = clf.predict_proba(X[y>0.5])[:, 1] # signal
-        decisions += [d1, d2] # add to list of classifier decision
-    
-    highest_decision = max(np.max(d) for d in decisions) # get maximum score
-    bin_edges = [] # list to hold bin edges
-    bin_edge = -0.1 # start counter for bin_edges
-    while bin_edge < highest_decision: # up to highest score
-    #while bin_edge < 1.0: # up to highest score
-        bin_edge += 0.1 # increment
-        bin_edges.append(bin_edge)
-
-    plt.figure() # make new figure 
-    plt.hist(decisions[0], # background in train set
-             bins=bin_edges, # lower and upper range of the bins
-             density=True, # area under the histogram will sum to 1
-             histtype='stepfilled', # lineplot that's filled
-             color='red', label='ggF (train)', # Background (train)
-             alpha=0.5 ) # half transparency
-    plt.hist(decisions[1], # background in train set
-             bins=bin_edges, # lower and upper range of the bins
-             density=True, # area under the histogram will sum to 1
-             histtype='stepfilled', # lineplot that's filled
-             color='blue', label='VBF (train)', # Signal (train)
-             alpha=0.5 ) # half transparency
-
-    hist_background, bin_edges = np.histogram(decisions[2], # background test
-                                              bins=bin_edges, # number of bins in function definition
-                                              density=True ) # area under the histogram will sum to 1
-    
-    scale = len(decisions[2]) / sum(hist_background) # between raw and normalised
-    err_background = np.sqrt(hist_background * scale) / scale # error on test background
-
-    width = 0.1 # histogram bin width
-    center = (bin_edges[:-1] + bin_edges[1:]) / 2 # bin centres
-    
-    plt.errorbar(x=center, y=hist_background, yerr=err_background, fmt='o', # circles
-                 c='red', label='ggF (test)' ) # Background (test)
-    
-    hist_signal, bin_edges = np.histogram(decisions[3], # siganl test
-                                          bins=bin_edges, # number of bins in function definition
-                                          density=True ) # area under the histogram will sum to 1
-    scale = len(decisions[3]) / sum(hist_signal) # between raw and normalised
-    err_signal = np.sqrt(hist_signal * scale) / scale # error on test background
-    
-    plt.errorbar(x=center, y=hist_signal, yerr=err_signal, fmt='o', # circles
-                 c='blue', label='VBF (test)' ) # Signal (test)
-    
-    plt.xlabel("BDT output") # write x-axis label
-    plt.ylabel("Arbitrary units") # write y-axis label
-    plt.legend() # add legend
-    #plt.show()
-    plt.savefig("BDT_plots/overtraining_check.pdf")
-    
-compare_train_test(bdt, X_train, y_train, X_test, y_test) # call compare_train_test
+plotting_BDT.compare_train_test(bdt, X_train, y_train, X_test, y_test) # plotting overtraining check
 
 # # Apply BDT to all the events
 # df["bdt_score"] = bdt.predict_proba(X)[:, 1]
@@ -238,10 +118,8 @@ print(X.dtypes)
 print(y.dtypes)
 
 # Convert the model into a ONNX file
-# Define entry type
 
-# bdt[0].name = "jet_features"
-input_name = bdt[0].name  # input name
+input_name = "jet_features"  # input name
 
 # initial_type = [("input", FloatTensorType([None, X.shape[1]]))]
 initial_type = [(input_name, FloatTensorType([None, X.shape[1]]))]
@@ -257,8 +135,6 @@ graph = onnx_model.graph
 # Original output name 
 original_output_name = graph.output[0].name  # "probabilities"
 
-
-# Crear inicializador para [1, 1]
 split_initializer = helper.make_tensor(
     name="split_sizes",
     data_type=TensorProto.INT64,
@@ -266,16 +142,14 @@ split_initializer = helper.make_tensor(
     vals=[1, 1]
 )
 
-# Nodo split (en opset ≥13, 'split' se pasa como entrada)
 split_node = helper.make_node(
     'Split',
-    inputs=[original_output_name, "split_sizes"],  # split_sizes es la nueva entrada
+    inputs=[original_output_name, "split_sizes"],  # split_sizes is the new entry
     outputs=['bdt_pggF', 'bdt_pVBF'],
     axis=1,
     name='SplitProbabilities'
 )
 
-# Agregar inicializador y nodo al grafo
 graph.initializer.append(split_initializer)
 graph.node.append(split_node)
 
@@ -289,7 +163,7 @@ for output_name in ["label", "probabilities"]:
     for output in graph.output:
         if output.name == output_name:
             graph.output.remove(output)
-            break  # salir del inner loop tras eliminar
+            break
 
 # for input in onnx_model.graph.input:
 #     print("Input name:", input.name)
---
+++ Analysis/VBF_ggF_separation/plotting_BDT.py
@@ -0,0 +1,147 @@
+import uproot
+import pandas as pd
+import awkward as ak
+import matplotlib.pyplot as plt
+import numpy as np
+import time # to measure time to analyse
+import seaborn as sns
+import joblib
+import h5py
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.metrics import roc_curve, auc
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+import onnx
+from onnx import helper, numpy_helper, TensorProto
+
+def correlation_plots(df):
+    correlation_matrix = df.corr()
+    plt.figure(figsize=(20, 16)) # make new figure
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", cbar_kws={"shrink": 0.8})
+    plt.title("Variables correlation matrix")
+    plt.tight_layout() 
+    plt.savefig("BDT_plots/correlation_matrix.pdf")
+    plt.figure()
+
+def ranking_variables_plot(features, importances, indices):
+    plt.figure(figsize=(8, 6))
+    plt.title("Importance of each variable")
+    plt.bar(range(len(features)), importances[indices], align="center")
+    plt.xticks(range(len(features)), features[indices], rotation=90)
+    plt.tight_layout()
+    plt.savefig("BDT_plots/importance_ranking_BDT.pdf")
+    plt.figure()
+    #plt.show()
+
+def roc_curve_plots(clf, X, y, X_test, y_test):
+    # we first plot the Neural Network output
+    # signal_decisions = bdt.decision_function(X[y>0.5]).ravel() # get probabilities on signal
+    # background_decisions = bdt.decision_function(X[y<0.5]).ravel() # get decisions on background
+
+    signal_decisions = clf.predict_proba(X[y>0.5])[:, 1] # get probabilities on signal
+    background_decisions = clf.predict_proba(X[y<0.5])[:, 1] # get decisions on background
+
+    plt.hist(background_decisions, color='red', label='ggF processes', 
+            histtype='step', # lineplot that's unfilled
+            density=True ) # normalize to form a probability density
+    plt.hist(signal_decisions, color='blue', label='VBF processes', 
+            histtype='step', # lineplot that's unfilled
+            density=True, # normalize to form a probability density
+            linestyle='--' ) # dashed line
+    plt.xlabel('BDT output') # add x-axis label
+    plt.ylabel('Arbitrary units') # add y-axis label
+    plt.legend() # add legend
+    #plt.show()
+    plt.savefig("BDT_plots/distributions_BDT_score.pdf")
+
+    # we then plot the ROC
+    plt.figure() # make new figure 
+
+    decisions = clf.decision_function(X_test).ravel() # get probabilities on test set
+
+    # Compute ROC curve and area under the curve
+    fpr, tpr, _ = roc_curve(y_test, # actual
+                            decisions ) # predicted
+
+    # Compute area under the curve for training set
+    roc_auc = auc(fpr, # false positive rate 
+                tpr) # true positive rate
+
+    plt.plot(fpr, tpr, label='ROC (area = %0.2f)'%(roc_auc)) # plot test ROC curve
+    plt.plot([0, 1], # x from 0 to 1
+            [0, 1], # y from 0 to 1
+            '--', # dashed line
+            color='grey', label='Luck')
+
+    plt.xlabel('False Positive Rate') # x-axis label
+    plt.ylabel('True Positive Rate') # y-axis label
+    plt.title('Receiver operating characteristic (ROC) curve') # title
+    plt.legend() # add legend
+    plt.grid() # add grid
+    #plt.show()
+    plt.savefig("BDT_plots/ROC_Curve.pdf")
+    plt.figure() # make new figure 
+
+### BDT Overtraining check
+def compare_train_test(clf, X_train, y_train, X_test, y_test):
+    decisions = [] # list to hold decisions of classifier
+    for X,y in ((X_train, y_train), (X_test, y_test)): # train and test
+        #d1 = clf.decision_function(X[y<0.5]).ravel() # background
+        #d2 = clf.decision_function(X[y>0.5]).ravel() # signal
+        d1 = clf.predict_proba(X[y<0.5])[:, 1] # background
+        d2 = clf.predict_proba(X[y>0.5])[:, 1] # signal
+        decisions += [d1, d2] # add to list of classifier decision
+    
+    highest_decision = max(np.max(d) for d in decisions) # get maximum score
+    bin_edges = [] # list to hold bin edges
+    bin_edge = -0.1 # start counter for bin_edges
+    while bin_edge < highest_decision: # up to highest score
+    #while bin_edge < 1.0: # up to highest score
+        bin_edge += 0.1 # increment
+        bin_edges.append(bin_edge)
+
+    plt.figure() # make new figure 
+    plt.hist(decisions[0], # background in train set
+             bins=bin_edges, # lower and upper range of the bins
+             density=True, # area under the histogram will sum to 1
+             histtype='stepfilled', # lineplot that's filled
+             color='red', label='ggF (train)', # Background (train)
+             alpha=0.5 ) # half transparency
+    plt.hist(decisions[1], # background in train set
+             bins=bin_edges, # lower and upper range of the bins
+             density=True, # area under the histogram will sum to 1
+             histtype='stepfilled', # lineplot that's filled
+             color='blue', label='VBF (train)', # Signal (train)
+             alpha=0.5 ) # half transparency
+
+    hist_background, bin_edges = np.histogram(decisions[2], # background test
+                                              bins=bin_edges, # number of bins in function definition
+                                              density=True ) # area under the histogram will sum to 1
+    
+    scale = len(decisions[2]) / sum(hist_background) # between raw and normalised
+    err_background = np.sqrt(hist_background * scale) / scale # error on test background
+
+    width = 0.1 # histogram bin width
+    center = (bin_edges[:-1] + bin_edges[1:]) / 2 # bin centres
+    
+    plt.errorbar(x=center, y=hist_background, yerr=err_background, fmt='o', # circles
+                 c='red', label='ggF (test)' ) # Background (test)
+    
+    hist_signal, bin_edges = np.histogram(decisions[3], # siganl test
+                                          bins=bin_edges, # number of bins in function definition
+                                          density=True ) # area under the histogram will sum to 1
+    scale = len(decisions[3]) / sum(hist_signal) # between raw and normalised
+    err_signal = np.sqrt(hist_signal * scale) / scale # error on test background
+    
+    plt.errorbar(x=center, y=hist_signal, yerr=err_signal, fmt='o', # circles
+                 c='blue', label='VBF (test)' ) # Signal (test)
+    
+    plt.xlabel("BDT output") # write x-axis label
+    plt.ylabel("Arbitrary units") # write y-axis label
+    plt.legend() # add legend
+    #plt.show()
+    plt.savefig("BDT_plots/overtraining_check.pdf")
